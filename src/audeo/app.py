c2491b2cdfe8a27312bfcc8991b40ac6add55124
Eclouf <165997982+Eclouf@users.noreply.github.com>
2024-08-23 16:16:38 +0200

    update app.py and add yt-dlp.py, spotDL

--- src/audeo/app.py
+++ src/audeo/app.py
@@ -1,25 +1,195 @@
 """
 Audeo is a GUI for downloading video and music from the Internet. It uses yt-dlp, ffmpeg, spotDL...
 """
+
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+import os 
+from toga import ImageView
+from toga.style.pack import CENTER
+import yt_dlp
+import threading
+from threading import Thread
+from queue import Queue
+import time
+
 
 
 class Audeo(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
+        # Création du layout principal avec alignement horizontal centré
+        main_box = toga.Box(style=Pack(direction=COLUMN, alignment=CENTER))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        # Création de l'image
+        title_image = toga.ImageView(image='./resources/audeo.png', style=Pack(padding=(0, 5)))
 
+        # Ajout de l'image au layout principal
+        main_box.add(title_image)
+
+        # Création des widgets
+        
+        self.url_input = toga.TextInput(style=Pack(flex=1), placeholder='Entrez une URL', on_change=self.on_url_input_change)
+        self.folder_button = toga.Button('Choisissez un dossier', on_press=self.select_folder)
+        # Créer une commande pour lancer l'opération
+        self.launch_command = toga.Command(self.launch_operation, 'Lancer', shortcut='l')
+
+        # Ajouter la commande au bouton
+        self.launch_button = toga.Button('Lancer', on_press=self.launch_command.action, enabled=False)
+
+        # Création des options avec un switch pour chaque option
+        self.play_list = toga.Switch('Liste de lecture', value=False)
+        self.thumbnail = toga.Switch('Miniature', value=False)
+        self.best_ext = toga.Switch('Meilleure extention', value=False)
+        self.meta = toga.Switch('Ajouter les métadonnées', value=False)
+        self.sub_title = toga.Switch('Sous-titre', value=False)
+        self.chapter = toga.Switch('Chapitre', value=False)
+        print(self.play_list.value)
+        
+        
+        # Définir le dossier de téléchargements par défaut
+        self.folder = os.path.expanduser('~/Downloads')
+        self.ffmpeg = os.path.join('./ressources/ffmpeg.exe')
+        
+        
+        # Création d'une ligne pour les widgets
+        row = toga.Box(style=Pack(direction=ROW))
+        row.add(self.url_input)
+        row.add(self.folder_button)
+        row.add(self.launch_button)
+
+        # Ajout du titre et de la ligne au layout principal
+        main_box.add(row)
+
+        # Création de deux colonnes pour les switches d'options
+        column1 = toga.Box(style=Pack(direction=COLUMN))
+        column2 = toga.Box(style=Pack(direction=COLUMN))
+
+        column1.add(self.thumbnail, self.play_list, self.best_ext)
+        column2.add(self.meta, self.sub_title, self.chapter)
+
+        # Création d'une ligne pour les colonnes
+        columns_row = toga.Box(style=Pack(direction=ROW))
+        columns_row.add(column1)
+        columns_row.add(column2)
+
+        # Ajout des colonnes au layout principal
+        main_box.add(columns_row)
+
+        #contenaire pour les téléchargement:
+        self.box = toga.Box(style=Pack(direction=COLUMN, alignment=CENTER))  # Créez une boîte pour contenir vos widgets
+        self.scroll = toga.ScrollContainer(content=self.box, style=Pack(direction=COLUMN, alignment=CENTER, flex=1))  # Ajoutez la boîte à ScrollContainer
+        main_box.add(self.scroll)
+        
+        # Création de la fenêtre principale
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
 
+        
+        
+    def select_folder(self, widget):
+        # Ouvrir le sélecteur de fichiers pour choisir un dossier
+        self.folder = self.main_window.select_folder_dialog(title="Choisissez un dossier")
+
+    def launch_operation(self, widget, event):
+        url = self.url_input.value
+        
+
+        
+        # Vérification de l'URL
+        if self.is_valid_url(url):
+            Opt_dlp = {}
+            print(f"L'URL {url} est valide.")
+            if self.play_list.value == False:
+                play_liste = True
+            else:
+                play_liste = False
+            thunm=self.thumbnail.value
+            best_ext=self.best_ext.value
+            meta=self.meta.value
+            sub_title=self.sub_title.value
+            chapter=self.chapter.value
+            print(play_liste,thunm,best_ext,meta,sub_title,chapter)
+
+            self.titre = toga.Label('Extraction des informations')
+            self.prcen = toga.Label('0.0 %')
+            #self.idplaylist = toga.Label('Liste de lecture : non')
+            self.idplay = toga.Label('0/0')
+            self.x = 0
+            
+
+            # Créez une boîte avec une direction ROW pour les labels
+            label_box = toga.Box(style=Pack(direction=COLUMN))
+            label_box.add(self.titre, self.idplay, self.prcen)#self.idplaylist,
+
+            self.progressbar = toga.ProgressBar(max=100)
+            self.progressbar.value=0
+
+            # Créez une boîte avec une direction COLUMN pour chaque ensemble de texte et de barre de progression
+            operation_box = toga.Box(style=Pack(direction=COLUMN))
+            operation_box.add(label_box)
+            operation_box.add(self.progressbar)
+
+            # Ajoutez operation_box à self.box
+            self.box.add(operation_box)
+            
+            queu = Queue()
+            queu_info = Queue()
+            ui_queue = Queue()
+           
+            
+            def run():
+                
+                
+                pass
+            
+            def info():
+                title, id, = queu_info.get()
+                self.titre.text = title
+                #self.idplaylist.text = liste
+                self.idplay.text = id
+                self.x=1
+                
+            
+            def update():
+                prct = queu.get()
+                print('prct = ',prct)
+                if prct != 100:
+                    self.progressbar.value = prct
+                    self.prcen.text = f'{self.progressbar.value}%'
+                else:
+                    self.progressbar.value = 100  # force la barre de progression à se remplir complètement
+                    self.prcen.text = '100%'
+                    self.x=0
+        
+            def message_def():
+                while not ui_queue.empty():
+                    message_type, message = ui_queue.get()
+                    print(message)
+                    if message_type == 'info':
+                        self.main_window.info_dialog('info', message)
+                    elif message_type == 'error':
+                        self.main_window.error_dialog('Erreur', message)
+                
+            # Ici, vous pouvez ajouter le code pour télécharger l'URL vers le dossier spécifié
+        else:
+            self.main_window.info_dialog('Erreur', 'L\'URL entrée n\'est pas valide.')
+
+    def on_url_input_change(self, widget):
+        # Activer ou désactiver le bouton de lancement en fonction de la validité de l'URL
+        self.launch_button.enabled = self.is_valid_url(self.url_input.value)
+
+    @staticmethod
+    def is_valid_url(url):
+        # Vérifier si l'URL est valide
+        return url.startswith('http://') or url.startswith('https://')
+
+
 def main():
     return Audeo()
+
+
+if __name__ == '__main__':
+    main().main_loop()
